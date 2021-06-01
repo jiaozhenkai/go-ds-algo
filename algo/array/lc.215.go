b7a3db4d47677479d65c47807a4c3f85f4e8f971
焦振凯 <jiaozhenkai@zhongruigroup.com>
2021-06-01 23:47:11 +0800

    add

--- algo/array/lc.215.go
+++ algo/array/lc.215.go
@@ -8,6 +8,20 @@ func findKthLargest(nums []int, k int) int {
 }
 
 func findKthLargest1(nums []int, k int) int {
-	sort.Ints(nums)
-	return nums[len(nums)-k]
+	numsLen := len(nums)
+	if numsLen == 0 {
+		return 0
+	}
+
+	if numsLen == 1 {
+		return nums[0]
+	}
+
+	target := numsLen - k
+
+	return find(nums, 1, numsLen-1, nums[0], target)
+}
+
+func find(nums []int, left, right, pivot, target int) int {
+	return 0
 }
